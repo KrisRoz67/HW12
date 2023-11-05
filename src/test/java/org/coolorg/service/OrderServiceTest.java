9155997172f943daba5bee3e4a665a790f19e9b9
Kristina Rozanova <zolina.kristina@gmail.com>
2023-11-05 14:25:35 +0200

    commit 5

--- src/test/java/org/coolorg/service/OrderServiceTest.java
+++ src/test/java/org/coolorg/service/OrderServiceTest.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
